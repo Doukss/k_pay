746081d5a2cca274847d76cd00504a2cff519ed3
Malick Mbodji <dmbodji297@gmail.com>
2026-08-15 15:12:30 +0000

    feat: add initial structure for gestion-locataires feature and remove unused button in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Button } from "./shared/components/ui/button"
 
 const App = () => {
   return (
     <div >
-      <Button variant="default" size="default">
-        Default Button
-      </Button>
+      
     </div>
   )
 }
