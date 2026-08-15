cf05a84b74c70ce5fe1cc47aad971a9b8c047e68
Malick Mbodji <dmbodji297@gmail.com>
2026-08-15 14:42:12 +0000

    chore: update dependencies and add new packages
    - Added new dependencies: @hookform/resolvers, @react-pdf/renderer, axios, date-fns, libphonenumber-js, react-hook-form, react-router-dom, recharts, sonner, zod, zustand.
    - Updated tsconfig.app.json to correct path alias from "@/ to "@/*".
    - Modified App.tsx to include a default button component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./shared/components/ui/button"
 
 const App = () => {
   return (
     <div >
+      <Button variant="default" size="default">
+        Default Button
+      </Button>
     </div>
   )
 }
